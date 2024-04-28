db2e61e2f51480ed703943ef1c8deabeeeb0c6f0
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-28 14:06:12 +0100

    Update settings.py

--- priestsite/settings.py
+++ priestsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m3*4jkwro-(2930$vg5n7f)&1c_rh57bpcur@7!o)*##+t@vkb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
